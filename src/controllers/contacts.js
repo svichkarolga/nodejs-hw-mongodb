a5d13224dad7eddd4e8c697e7031a36862503b8f
svichkar olga <syns41@gmail.com>
2024-12-09 18:57:00 +0200

    hw done

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,10 @@
-import { getAllContacts, getContactsById } from '../services/contacts.js';
+import {
+  getAllContacts,
+  getContactsById,
+  createContact,
+  patchContact,
+  deleteContact,
+} from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res) => {
@@ -21,4 +27,37 @@ export const getContactsByIdController = async (req, res, next) => {
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
   });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: contact,
+  });
+};
+
+export const patchContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await patchContact(contactId, req.body);
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+  res.json({
+    status: 200,
+    message: `Successfully patched a contact!`,
+    data: result.contact,
+  });
+};
+
+export const deleteContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await deleteContact(contactId);
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+  res.status(204).send();
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,32 @@ export const getContactsById = async (contactId) => {
   const contact = await ContactCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await ContactCollection.create(payload);
+  return contact;
+};
+
+export const patchContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactCollection.findOneAndDelete({ _id: contactId });
+  return contact;
+};
