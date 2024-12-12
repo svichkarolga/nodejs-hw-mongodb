6f2cd86f4aabade288dd9da319a7cf785fe524c6
svichkar olga <syns41@gmail.com>
2024-12-12 02:13:11 +0200

    first commit

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,6 +7,11 @@ import {
   deleteContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 
 const router = Router();
 
@@ -18,5 +23,17 @@ router.post('/contacts', ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 
 export default router;
