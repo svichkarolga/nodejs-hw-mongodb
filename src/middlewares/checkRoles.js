ed83325cdcba0f021e1ab3ab577c34e1dcbdbbdb
svichkar olga <syns41@gmail.com>
2024-12-22 00:25:08 +0200

    fix added

--- src/middlewares/checkRoles.js
+++ src/middlewares/checkRoles.js
@@ -1,65 +1,7 @@
-// import createHttpError from 'http-errors';
-// import { ContactCollection } from '../db/models/Contact.js';
-// import { ROLES } from '../constants/index.js';
-
-// export const checkRoles =
-//   (...roles) =>
-//   async (req, res, next) => {
-//     const { user } = req;
-
-//     if (!user) {
-//       next(createHttpError(401, 'Unauthorized'));
-//       return;
-//     }
-
-//     const { role } = user;
-
-//     // Якщо роль користувача — USER
-//     if (roles.includes(ROLES.USER) && role === ROLES.USER) {
-//       const { contactId } = req.params;
-
-//       // Якщо contactId не переданий
-//       if (!contactId) {
-//         next(createHttpError(403, 'Forbidden: Access denied'));
-//         return;
-//       }
-
-//       // Перевірка, чи належить контакт користувачу
-//       const contact = await ContactCollection.findOne({
-//         _id: contactId,
-//         userId: user._id,
-//       });
-
-//       if (contact) {
-//         next();
-//         return;
-//       }
-
-//       next(
-//         createHttpError(403, 'Forbidden: Contact not found or access denied'),
-//       );
-//       return;
-//     }
-
-//     // Якщо роль користувача — дозволена (але не USER)
-//     if (roles.includes(role)) {
-//       next();
-//       return;
-//     }
-
-//     // Якщо роль не дозволена
-//     next(createHttpError(403, 'Forbidden: Access denied'));
-//   };
-
 import createHttpError from 'http-errors';
 import { ContactCollection } from '../db/models/Contact.js';
 import { ROLES } from '../constants/index.js';
 
-/**
- * Middleware для перевірки ролей та прав доступу.
- * @param {...string} roles - дозволені ролі.
- * @returns {function} - middleware для перевірки доступу.
- */
 export const checkRoles =
   (...roles) =>
   async (req, res, next) => {
