2e3f1770aea6fd56fd7c97387fd3cd776040cdec
svichkar olga <syns41@gmail.com>
2024-12-24 00:46:03 +0200

    add pattern regexp for email

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,3 +5,5 @@ export const SORT_ORDER = {
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
+
+export const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
