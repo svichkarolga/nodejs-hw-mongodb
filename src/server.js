1934d2e6db8b9d64562980f045de5cbac0e25f2b
svichkar olga <syns41@gmail.com>
2025-01-12 01:35:31 +0200

    corr mist

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { logger } from './middlewares/logger.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 const PORT = Number(process.env.PORT) || 3000;
@@ -22,6 +23,8 @@ export const setupServer = () => {
   app.use('*', notFoundHandler);
   app.use(errorHandler);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
